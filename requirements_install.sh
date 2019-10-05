501fe72b930d004057a315eb3a92d1678f536d91
Chenna Keshava B S <chenna@ualberta.ca>
2019-10-05 20:41:02 +0530

    Update requirements_install.sh

--- requirements_install.sh
+++ requirements_install.sh
@@ -1,7 +1,13 @@
 cd /tmp
 curl -O https://repo.anaconda.com/archive/Anaconda3-2019.03-Linux-x86_64.sh
+bash Anaconda3-2019.03-Linux-x86_64.sh
 source ~/.bashrc
+printf "installed anaconda" 
 cd ~
 conda create -n tensorflow_env tensorflow
 conda activate tensorflow_env
 conda install -c conda-forge librosa
+printf "installed librosa"
+
+conda install -c conda-forge matplotlib
+printf "installed matplotlib"
