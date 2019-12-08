1811688a330cca605a958b0ad796bbbc9211a2c1
Chenna Keshava B S <chenna@ualberta.ca>
2019-12-08 04:37:29 +0000

    cnn4 BN

--- cnn4_max_pool_model_with_batchnorm.py
+++ cnn4_max_pool_model_with_batchnorm.py
@@ -1,3 +1,6 @@
+from keras.layers import BatchNormalization
+from datetime import datetime
+import pickle
 from keras.utils import to_categorical
 from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
 import matplotlib.pyplot as plt
@@ -14,7 +17,7 @@ from keras.utils import np_utils
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import KFold
 from sklearn.preprocessing import LabelEncoder
-
+from sklearn.model_selection import train_test_split
 from keras.utils.vis_utils import plot_model
 
 
@@ -38,63 +41,72 @@ for raga in RAGAS:
         data.append(x)
         # np.append(data, x, axis=-1)
 
-# print(labels)
 print(len(labels))
 print(len(data))
 
-# encode class values as integers
-#encoder = LabelEncoder()
-#encoder.fit(labels)
-#encoded_Y = encoder.transform(labels)
-# convert integers to dummy variables (i.e. one hot encoded)
-#dummy_y = np_utils.to_categorical(encoded_Y)
-labels = to_categorical(labels)
+X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size=0.25, random_state=42)
+
+y_train = to_categorical(y_train)
+y_test = to_categorical(y_test)
+
 
 
 def baseline_model():
     model = Sequential()
     model.add(Conv2D(32, (3, 3), input_shape=(480, 640, 3)))
     model.add(Activation('relu'))
+    model.add(BatchNormalization())
     model.add(MaxPooling2D(pool_size=(4, 4)))
 
-    model.add(Conv2D(32, (3, 3)))
+    model.add(Conv2D(64, (3, 3)))
     model.add(Activation('relu'))
+    model.add(BatchNormalization())
     model.add(MaxPooling2D(pool_size=(4, 4)))
 
-    model.add(Conv2D(32, (3, 3)))
+    model.add(Conv2D(128, (3, 3)))
     model.add(Activation('relu'))
+    model.add(BatchNormalization())
     model.add(MaxPooling2D(pool_size=(4, 4)))
 
 
-    model.add(Conv2D(32, (3, 3)))
+    model.add(Conv2D(128, (3, 3)))
     model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(4, 4)))
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+
 
     model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
-    model.add(Dense(32))
-    model.add(Activation('relu'))
-#    model.add(Dropout(0.5))
+    #model.add(Dense(32))
+    #model.add(Activation('relu'))
 
 #    model.add(Dense(32))
 #    model.add(Activation('relu'))
-#    model.add(Dropout(0.5))
+#    model.add(Dropout(0.2))
 
     model.add(Dense(NUM_RAGAS))
     model.add(Activation('softmax'))
 
     model.compile(loss='categorical_crossentropy',
-                  optimizer=sgd(),
+                  optimizer='rmsprop',
                   metrics=['accuracy'])
 
     # print(model.summary())
     # plot_model(model, to_file='model_plot.png', show_shapes=True, show_layer_names=True)
     return model
 
+
+#logdir = "logs/scalars/" + str(datetime.now())
+#tensorboard_callback = keras.callbacks.Tensorboard(logdir=logdir)
 model = baseline_model()
 print(model.summary())
-history=model.fit(np.array(data), np.array(labels), validation_split=0.20, epochs=50, verbose=2)
 
-model.save('cnn2.h5')
+#history=model.fit(np.array(X_train), np.array(y_train), validation_split=0.0, epochs=2, verbose=2, callbacks=[tensorboard_callback])
+history=model.fit(np.array(X_train), np.array(y_train), shuffle=True, validation_split=0.20, epochs=200, verbose=2)
+score = model.evaluate(np.array(X_test), np.array(y_test))
+print(score)
+
+model.save('fully_convolutional_model_4-layers_with_batchnorm.h5')
 #estimator = KerasClassifier(build_fn=baseline_model, epochs=1, batch_size=5, verbose=2)
 #kfold = KFold(n_splits=10, shuffle=True)
 #results = cross_val_score(estimator, np.array(data), np.array(labels), cv=kfold)
@@ -102,9 +114,9 @@ model.save('cnn2.h5')
 
 
 # save history to a file
-f = open("history", "w")
-f.write(history)
-f.close()
+with open('./trainHistoryDict_maxpool_dropout_cnn4_batchnorm', 'wb') as file_pi:
+    pickle.dump(history, file_pi)
+
 
 #print(history.history.keys())
 # summarize history for accuracy
